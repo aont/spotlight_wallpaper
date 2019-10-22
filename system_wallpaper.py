c44f801630de2736677af096d613d3cfb12e3cde
aont <>
2019-10-22 17:54:28 +0900

    system_wallpaper.py: pure python way

--- system_wallpaper.py
+++ system_wallpaper.py
@@ -1,64 +1,50 @@
 #!/usr/bin/env python
-import ctypes
 
-_dll = ctypes.WinDLL('system_wallpaper.dll')
-_dll.system_wallpaper.restype = ctypes.c_wchar_p
-_dll.system_wallpaper.argtypes = []
+import comtypes
+import comtypes.client
 
-print(_dll.system_wallpaper())
+CLSID_DesktopWallpaper = comtypes.GUID("{C2CF3110-460E-4FC1-B9D0-8A1C0C9CC4BD}")
+CLSID_IDesktopWallpaper = comtypes.GUID("{B92B56A9-8B55-4E14-9A89-0199BBB6F93B}")
 
-# ).value.decode("UTF-16LE"))
+mod = comtypes.client.GetModule('shobjidl.tlb')
+
+dw = comtypes.client.CreateObject(CLSID_DesktopWallpaper, interface=mod.IDesktopWallpaper)
+
+monitorid = dw.GetMonitorDevicePathAt(0)
+# print(monitorid)
+wppath = dw.GetWallpaper(monitorid)
+print(wppath)
+
+dw.Release()
 
 
-#
-# import comtypes
-# import comtypes.client
-# import ctypes
+# import pythoncom
 
-# # import win32com
-# # import win32com.client
+# # for i in dir(pythoncom):
+#   # print("%s:%s:%s"%(i, pythoncom.get(i), type(pythoncom.get(i))))
+#   #print("%s"%(i))
+# CLSID_DesktopWallpaper = "{C2CF3110-460E-4FC1-B9D0-8A1C0C9CC4BD}"
+# CLSID_IDesktopWallpaper = "{B92B56A9-8B55-4E14-9A89-0199BBB6F93B}"
 
-# # win32com.client.
+# PYIID_DesktopWallpaper = pythoncom.MakeIID(CLSID_DesktopWallpaper)
+# PYIID_IDesktopWallpaper = pythoncom.MakeIID(CLSID_IDesktopWallpaper)
 
-# CLSID_DesktopWallpaper = comtypes.GUID("{C2CF3110-460E-4FC1-B9D0-8A1C0C9CC4BD}")
-# CLSID_IDesktopWallpaper = comtypes.GUID("{B92B56A9-8B55-4E14-9A89-0199BBB6F93B}")
-# # desktop_wallpaper = comtypes.client.CreateObject(CLSID_DesktopWallpaper, interface=CLSID_IDesktopWallpaper)
-
-# hr = comtypes.CoInitialize()
-# #(
-# # idesktop_wallpaper = 
-# comtypes.
-# desktop_wallpaper = comtypes.CoCreateInstance(CLSID_DesktopWallpaper, clsctx=comtypes.CLSCTX_ALL)
-# # CoCreateInstance
-
-# monitorid = comtypes.c_wchar_p()
-# desktop_wallpaper.GetMonitorDevicePathAt(0, monitorid)
-# print(monitorid.value)
-# desktop_wallpaper.Release()
-# # idesktop_wallpaper.Release()
-
-# comtypes.CoUninitialize()
-#####################
-
-# import ctypes
-# user32_dll = ctypes.WinDLL('user32.dll')
-# user32_dll.SystemParametersInfo.restype = ctypes.c_bool
-# user32_dll.SystemParametersInfo.argtypes = [ctypes.c_uint, ctypes.c_uint, ctypes.c_void_p, ctypes.c_uint]
-
-# SPI_GETDESKWALLPAPER=0x0073
-
-# user32_dll.SystemParametersInfo(SPI_GETDESKWALLPAPER, 
-
-#import ctypes
+# import _shobjidl
 # import win32com.client
+# pythoncom.CoInitialize()
 
-# "IDesktopWallpaper"
-# "{B92B56A9-8B55-4E14-9A89-0199BBB6F93B}"
-# "{C2CF3110-460E-4FC1-B9D0-8A1C0C9CC4BD}"
-#desktop_wallpaper = win32com.client
 
-# win32com.client.
-#wallpaper = ctypes.c_char_p()
-#desktop_wallpaper.GetWallpaper(None, wallpaper)
-#print(wallpaper.value.decote('utf-8'))
-# win32com.client.
+
+# dw = pythoncom.CoCreateInstance(CLSID_DesktopWallpaper, None, pythoncom.CLSCTX_ALL, PYIID_IDesktopWallpaper)
+# # dw = pythoncom.CoCreateInstance(CLSID_DesktopWallpaper, None, pythoncom.CLSCTX_ALL, pythoncom.IID_IUnknown)
+# # dw = win32com.client.CastTo(dw, PYIID_IDesktopWallpaper)
+
+# for i in dir(dw):
+#   print(i)
+# print(dw)
+# # monitorid = dw.GetMonitorDevicePathAt(0)
+# # print(monitorid)
+# # dw = pythoncom.CoCreateInstance(CLSID_DesktopWallpaper, iunk, pythoncom.CLSCTX_ALL, PYIID_IDesktopWallpaper)
+# # dw = pythoncom.CoCreateInstance(PYIID_DesktopWallpaper, None, pythoncom.CLSCTX_ALL, PYIID_IDesktopWallpaper)
+# dw.Release()
+# pythoncom.CoUninitialize()
