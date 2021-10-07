9482787050e6df0a4daf3d0d65aa3673f7d8235c
aont <220471+aont@users.noreply.github.com>
2021-10-07 15:50:00 +0900

    no need to use tlb

--- gen_tlb.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set SHOBJIDL_IDL=c:/msys64/mingw64/x86_64-w64-mingw32/include/shobjidl.idl
-midl.exe "%SHOBJIDL_IDL%" /tlb shobjidl.tlb
--- system_wallpaper.py
+++ system_wallpaper.py
@@ -2,13 +2,42 @@
 
 import comtypes
 import comtypes.client
+import ctypes
+import ctypes.wintypes
+
+class IDesktopWallpaper(comtypes.IUnknown):
+    _iid_ = comtypes.GUID('{B92B56A9-8b55-4e14-9a89-0199bbb6f93b}')
+    _methods_ = (
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'SetWallpaper',
+            (['in', 'unique'], ctypes.wintypes.LPCWSTR, "monitorID"),
+            (['in'], ctypes.wintypes.LPWSTR, "wallpaper"),
+        ),
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'GetWallpaper',
+            (['in', 'unique'], ctypes.wintypes.LPCWSTR, "monitorID"),
+            (['out', 'string'], ctypes.POINTER(ctypes.wintypes.LPWSTR), "wallpaper"),
+        ),
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'GetMonitorDevicePathAt',
+            (['in'], ctypes.wintypes.UINT, "monitorIndex"),
+            (['out', 'string'], ctypes.POINTER(ctypes.wintypes.LPWSTR), "monitorID"),
+        ),
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'GetMonitorDevicePathCount', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'GetMonitorRECT', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'SetBackgroundColor', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'GetBackgroundColor', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'SetPosition', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'GetPosition', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'SetSlideshow', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'GetSlideshow', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'SetSlideshowOptions', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'GetSlideshowOptions', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'AdvanceSlideshow', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'GetStatus', ), # todo
+        comtypes.COMMETHOD([], comtypes.HRESULT, 'Enable', ), # todo
+    )
 
 CLSID_DesktopWallpaper = comtypes.GUID("{C2CF3110-460E-4FC1-B9D0-8A1C0C9CC4BD}")
-CLSID_IDesktopWallpaper = comtypes.GUID("{B92B56A9-8B55-4E14-9A89-0199BBB6F93B}")
 
-mod = comtypes.client.GetModule('shobjidl.tlb')
-
-dw = comtypes.client.CreateObject(CLSID_DesktopWallpaper, interface=mod.IDesktopWallpaper)
+dw = comtypes.client.CreateObject(CLSID_DesktopWallpaper, interface=IDesktopWallpaper)
 
 monitorid = dw.GetMonitorDevicePathAt(0)
 
